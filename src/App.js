0a931551612057cfa736984c1e20176a7dacded3
Gavin Webster <gw@exemail.com.au>
2021-02-08 23:50:05 +1100

    Added Array of Posts

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './styles.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
